f3f64e0fedeb3d14d2eb838dce7bf84f58f5f4ce
Vinícius Santana <52092114+VinniVicius@users.noreply.github.com>
2020-07-02 02:18:53 -0300

    FAQ e Course Fee section
    FAQ e Course Fee section finalizados

--- js/slider.js
+++ js/slider.js
@@ -18,9 +18,28 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active_slide", "");
-  }
   slides[slideIndex-1].style.display = "flex";  
-  dots[slideIndex-1].className += " active_slide";
+}
+
+var slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+function plusSlides2(n2) {
+  showSlides2(slideIndex2 += n2);
+}
+
+function currentSlide2(n2) {
+  showSlides2(slideIndex2 = n2);
+}
+
+function showSlides2(n2) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides2");
+  var dots = document.getElementsByClassName("dot");
+  if (n2 > slides.length) {slideIndex2 = 1}    
+  if (n2 < 1) {slideIndex2 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slides[slideIndex2-1].style.display = "flex";  
 }
